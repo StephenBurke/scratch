a763789d50d4e47d0d63195f87e5f75d5328183a
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2020-01-27 14:18:39 -0600

    added walrus example

--- python.py
+++ python.py
@@ -18,3 +18,27 @@ def calc_factorial(x):
 num = 4
 print("The factorial of", num, "is", calc_factorial(num))		
 
+##########
+# walrus example
+
+sample_data = [ 
+    {"userId": 1,  "name": "rahul", "completed": False}, 
+    {"userId": 1, "name": "rohit", "completed": False}, 
+    {"userId": 1,  "name": "ram", "completed": False}, 
+    {"userId": 1,  "name": "ravan", "completed": True} 
+] 
+  
+print("With Python 3.8 Walrus Operator:")  
+for entry in sample_data:  
+    if name := entry.get("name"): 
+        print(f'Found name: "{name}"') 
+  
+print("Without Walrus operator:") 
+for entry in sample_data: 
+    name = entry.get("name") 
+    if name: 
+        print(f'Found name: "{name}"')
+
+print('study')
+
+
